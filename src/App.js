de10cc99406579a4cb4a53734ce1aee4cf5b91bb
sai charan <saicharan0662@gmail.com>
2021-10-15 10:19:56 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="md:hidden">
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/explore' component={Explore} />
-        <Route path='/activity' component={Activity} />
-        <Route path='/profile' component={Profile} />
-      </Switch>
-      <Tabs />
-    </div>
+    <>
+      <div className="md:hidden">
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/explore' component={Explore} />
+          <Route path='/activity' component={Activity} />
+          <Route path='/profile' component={Profile} />
+        </Switch>
+        <Tabs />
+      </div>
+      <div className="hidden md:block w-screan text-center font-bold text-xl">
+        Sorry we are only on small screans <br />
+        width should be less than 768px
+      </div>
+    </>
   );
 }
 
